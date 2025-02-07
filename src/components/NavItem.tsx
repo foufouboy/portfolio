cd05be149c8da5c1d4e6fafd8e3d1c070b12f63b
foufouboy <cissaclement@gmail.com>
2025-02-07 15:20:11 +0100

    experiments on theme

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,7 +1,7 @@
 function NavItem({ text, link, active }) {
   return (
     <li
-      className={`font-[500] transition-colors duration-300 hover:text-red-500 ${active && "text-red-500"} `}
+      className={`font-[600] transition-colors duration-300 hover:text-red-500 ${active && "text-primary-red"} `}
     >
       <a href={link}>{text}</a>
     </li>
